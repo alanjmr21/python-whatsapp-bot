ebdd783d9dc7850c8ce3227b49a9ec2654da2a65
Alan Muñoz <alanjmr206@gmail.com>
2023-12-30 15:10:50 -0700

    comment signature required in webhoook endpoint

--- app/views.py
+++ app/views.py
@@ -78,7 +78,7 @@ def verify():
 
 
 @webhook_blueprint.route("/webhook", methods=["POST", "GET"])
-@signature_required
+# @signature_required
 def webhook():
     if request.method == "GET":
         return verify()
